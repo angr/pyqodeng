449ae5e4e04eef1f20b9f9f039ad2cf890e5ac65
Fish <fishw@asu.edu>
2026-02-08 23:06:57 -0700

    Remove the pyqodeng namespace.

--- pyqodeng/__init__.py
+++ pyqodeng/__init__.py
@@ -3,3 +3,5 @@
 pyQodeNG is a source code editor widget for Python Qt 6 (PySide6/PyQt6). It is a fork of pyQode.
 The original pyQode can be found at https://github.com/pyQode/pyqode.core.
 """
+
+from . import core
